6f850baa0d77b8e576ef2b3b889f656c94583b8f
jchillah <jchillah@gmail.com>
2024-05-22 16:37:29 +0200

    added button to show when dispose ran

--- lib/src/features/Screens/main_screen/presentation/main_screen.dart
+++ lib/src/features/Screens/main_screen/presentation/main_screen.dart
@@ -4,61 +4,54 @@ class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  MainScreenState createState() => MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int counter = 0;
+class MainScreenState extends State<MainScreen> {
+  int _counter = 0;
 
   @override
   void initState() {
     super.initState();
-    print("initState function is running now");
+    debugPrint('initState() wurde aufgerufen.');
+  }
+
+  @override
+  void dispose() {
+    debugPrint('dispose() wurde aufgerufen.');
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 2;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Build function is running");
+    debugPrint("build() wurde aufgerufen");
     return Scaffold(
       appBar: AppBar(
-        title: const Text("TS 5.0.2 Widget Lifecycle"),
+        title: const Text('TS 5.0.2 Widget Lifecycle'),
       ),
-      body: Row(
-        children: [
-          Column(
-            children: [
-              const Text("Counter wird immer um 2 erhöht"),
-              OutlinedButton(
-                onPressed: () {
-                  setState(() {
-                    counter += 2;
-                    print(
-                        "Counter wurde um 2 erhöht und steht jetzt bei $counter");
-                  });
-                },
-                child: Text("counter is $counter"),
-              ),
-              const Text("Counter wird immer um 1 verringert"),
-              OutlinedButton(
-                onPressed: () {
-                  setState(() {
-                    counter--;
-                    print(
-                        "Counter wurde um 1 verringert und steht jetzt bei $counter");
-                  });
-                },
-                child: Text("counter is $counter"),
-              )
-            ],
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Zahl: $_counter',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Klick mich'),
+            ),
+            ElevatedButton(onPressed: dispose, child: const Text("Beenden"))
+          ],
+        ),
       ),
     );
   }
-}
-
-@override
-void dispose() {
-  print("dispose ran");
-  dispose();
 }
